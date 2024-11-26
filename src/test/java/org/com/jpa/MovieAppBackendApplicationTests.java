97d0b4354c2162afc6160e3606de396fe4e51751
Godcoder696 <117354227+Godcoder696@users.noreply.github.com>
2024-11-26 21:37:36 +0530

    backend initialized

---
+++ src/test/java/org/com/jpa/MovieAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.com.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
